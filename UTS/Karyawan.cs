41a411b5299171728c90d60beaddcd593925aebf
lasmitatrian <lasmitatriana36@gmail.com>
2019-05-01 19:30:22 +0700

    UTS Pemrograman

---
+++ UTS/Karyawan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTS
+{
+    class Karyawan
+    {
+        public Karyawan()
+        {
+        }
+        public Karyawan(string nik, string nama, int gajibulanan)
+        {
+            if (gajibulanan < 0)
+            {
+                Console.WriteLine("gaji harus diatas 0!");
+            }
+            else
+            {
+                Nik = nik;
+                Nama = nama;
+                GajiBulanan = gajibulanan;
+
+
+            }
+        }
+
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public double GajiBulanan { get; set; }
+    }
+
+}
---
+++ UTS/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan krywan1 = new Karyawan();
+            krywan1.Nik = "190302124";
+            krywan1.Nama = "Paijo";
+            krywan1.GajiBulanan = 3000000;
+
+            Karyawan krywan2 = new Karyawan();
+            krywan2.Nik = "190302125";
+            krywan2.Nama = "Jono";
+            krywan2.GajiBulanan = 2000000;
+
+            Console.WriteLine("No. \tNik/Nama\tGaji Bulanan");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("1.\t{0} {1}\t\t{2}", krywan1.Nik, krywan1.Nama, krywan1.GajiBulanan);
+            Console.WriteLine("2.\t{0} {1}\t\t{2}", krywan2.Nik, krywan2.Nama, krywan2.GajiBulanan);
+            krywan1.GajiBulanan += krywan1.GajiBulanan * 0.1;
+            krywan2.GajiBulanan += krywan2.GajiBulanan * 0.1;
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine(" yee kenaikan gaji 10%");
+            Console.WriteLine("\n");
+            Console.WriteLine("No. \tNik/Nama\t\tGaji Bulanan");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("1.\t{0} {1}\t\t{2}", krywan1.Nik, krywan1.Nama, krywan1.GajiBulanan);
+            Console.WriteLine("2.\t{0} {1}\t\t{2}", krywan2.Nik, krywan2.Nama, krywan2.GajiBulanan);
+
+            Console.ReadKey();
+
+        }
+    }
+}
